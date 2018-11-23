816586e546f37cee85583d2ea6299de0b7db1faa
Urs Nussbaumer <urs.nussbaumer@ict-bz.ch>
2018-11-23 12:31:50 +0100

    project finished.

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -2,15 +2,16 @@
 {
     partial class Form1
     {
+
         /// <summary>
-        /// Erforderliche Designervariable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Verwendete Ressourcen bereinigen.
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,41 +21,114 @@
             base.Dispose(disposing);
         }
 
-        #region Vom Windows Form-Designer generierter Code
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// Erforderliche Methode für die Designerunterstützung.
-        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.buttonGuess = new System.Windows.Forms.Button();
+            this.textBoxMessage = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(643, 363);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Zahl (1...100)";
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(119, 12);
+            this.textBoxGuess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxGuess.MaxLength = 3;
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(192, 23);
+            this.textBoxGuess.TabIndex = 1;
+            this.textBoxGuess.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBoxGuess_KeyPress);
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.Location = new System.Drawing.Point(320, 10);
+            this.buttonGuess.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(100, 27);
+            this.buttonGuess.TabIndex = 2;
+            this.buttonGuess.Text = "Prüfen";
+            this.buttonGuess.UseVisualStyleBackColor = true;
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // textBoxMessage
+            // 
+            this.textBoxMessage.Location = new System.Drawing.Point(119, 62);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxMessage.Name = "textBoxMessage";
+            this.textBoxMessage.Size = new System.Drawing.Size(192, 23);
+            this.textBoxMessage.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ausgabe";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(320, 97);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(100, 28);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Beenden";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.buttonGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(436, 129);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMessage);
+            this.Controls.Add(this.buttonGuess);
+            this.Controls.Add(this.textBoxGuess);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zahlenraten";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxGuess;
+        private System.Windows.Forms.Button buttonGuess;
+        private System.Windows.Forms.TextBox textBoxMessage;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonExit;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -12,9 +12,78 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        #region variables 
+
+        // Variable, welche die generierte Zufallszahl aufnimmt 
+        int _number = 0;
+
+        // Variable,in welcher die Anzahl Versuche gespeichert werden
+        int _countOfTrials = 0;
+
+        // Liste, welche alle vom Benutzer eingegebenen Zahlen speichert 
+        List<int> _guessed = new List<int>();
+
+        #endregion
+
+        #region constructors 
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        #endregion 
+
+        #region event handler 
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxGuess.Text))
+                return;
+
+            int guess = Convert.ToInt32(textBoxGuess.Text);
+
+            if (_guessed.Contains(guess))
+                MessageBox.Show("Mit dieser Zahl haben Sie es schon einmal versucht.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                _guessed.Add(guess);
+
+            if (guess.Equals(_number))
+            {
+                textBoxMessage.Text = "Zahl stimmt! (" + _countOfTrials.ToString() + " Versuche)";
+                buttonGuess.Enabled = false;
+                return;
+            }
+
+            if (guess > _number)
+                textBoxMessage.Text = "Zahl zu gross!";
+            else if (guess < _number)
+                textBoxMessage.Text = "Zahl zu klein!";
+
+            textBoxGuess.Focus();
+            textBoxGuess.SelectAll();
+
+            _countOfTrials++;
+        }
+
+        private void textBoxGuess_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // only allow the user to add numbers and {Backspace}
+            if (!char.IsDigit(e.KeyChar) && !e.KeyChar.Equals('\b'))
+                e.Handled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Zufallszahl zwischen 1 und 100 generieren 
+            _number = (new Random().Next(1, 101));
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        #endregion 
     }
 }
